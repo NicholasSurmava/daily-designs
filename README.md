a6299f6b360697b814d0189794545bd626f3d5ae
Nicholas Surmava <nicholas.surmava@gmail.com>
2018-04-17 20:50:09 -0500

    Cleaned up project structure and added some gulp tasks. Learning gulp!

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ##To-Do:
-- subdomain as daily-design.nicholassurmava.com
 - index at root will display a grid of designs. Link to each individual designs.
 - simple host w/ nginx
+    - subdomain at DO. daily-design.nicholassurmava.com
 - Create a new design ~~daily~~ semi-daily.
+- Setup Gulp
+    - Minify CSS, JS files
+    - deploy to VPS omitting unneeded dirs (mockup, etc.)
+    - automate new design creation with gulp
+    - look into injecting the proper links into main index.html automatically
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,53 @@
-var gulp = require('gulp');
+'use strict';
+ 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sync = require('browser-sync');
+const min = require('gulp-uglify');
+const min_img = require('gulp-imagemin');
+const prefix = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+ 
+// SASS to CSS
+gulp.task('sass', function () {
+  return gulp.src('./src/styles/sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./src/styles/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/styles/sass/**/*.scss', ['sass']);
+});
+
+// Minify JS
+gulp.task('min', function () {
+
+});
+
+// Minify Images
+gulp.task('min_img', function () {
+
+});
+
+// auto prefixer
+gulp.task('prefix', function() {
+
+});
+
+// Dev Server
+gulp.task('dev', function () {
+  
+});
+
+// Deploy
+gulp.task('deploy', function () {
+
+});
+
+// Daily Design
+gulp.task('dd', function () {
+
 });
